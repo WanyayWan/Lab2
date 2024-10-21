0316a547366e2ff39310d88ce7e301573ca1b158
WanyayWan <yanyan.23@ichat.sp.edu.sg>
2024-10-21 15:29:56 +0800

    Hi i just updated

--- bmi.py
+++ bmi.py
@@ -1,4 +1,12 @@
 def calculate_bmi(height, weight):
  print("Height = " + str(height))
  print("Weight = " + str(weight))
-calculate_bmi(weight=57, height=1.80)
+ 
+ #Add code here to calculate BMI
+ bmi = weight/height*height
+
+ 
+ #Add code here to display calculate BMI
+ print("Your bmi is =" + str(bmi))
+
+calculate_bmi(weight=57, height=1.73)
